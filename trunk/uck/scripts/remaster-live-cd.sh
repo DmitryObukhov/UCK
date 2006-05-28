400c3c436fcb6c6a9735d82446a340fd337b4771
krzysiek <krzysiek@6cad917b-6c01-41db-971f-40384d2b65c3>
2006-05-28 10:59:59 +0000

    Preparing for splitting into functions.

--- trunk/uck/scripts/remaster-live-cd.sh
+++ trunk/uck/scripts/remaster-live-cd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ISO_IMAGE="$1"
-CUTOMIZE_DIR="$2"
+CUSTOMIZE_DIR="$2"
 ISO_MOUNT_DIR=~/tmp/iso-source2
 SQUASHFS_IMAGE="$ISO_MOUNT_DIR/casper/filesystem.squashfs"
 SQUASHFS_MOUNT_DIR=~/tmp/squashfs-source
@@ -17,7 +17,7 @@ NEW_FILES_DIR=~/tmp/remaster-new-files
 LIVECD_ISO_DESCRIPTION="Remastered LiveCD"
 
 echo "Starting CD remastering on " `date`
-echo "Cutomization dir=$CUTOMIZE_DIR" 
+echo "Cutomization dir=$CUSTOMIZE_DIR" 
 
 function usage()
 {
@@ -39,17 +39,31 @@ function removeDirectory()
 	rm -rf "$DIR_TO_REMOVE"
 }
 
+function unpack_initrd()
+{
+	#not ready yet
+	failure "Not implemented"
+	cat $1 | cpio -i 
+}
+
+function pack_initrd()
+{
+	#not ready yet
+	failure "Not implemented"
+	find | cpio -H newc -o | gzip >initrd.gz
+}
+
 if [ -z "$ISO_IMAGE" ]; then
 	usage
 	exit 1
 fi
 
-if [ -z "$CUTOMIZE_DIR" ]; then
+if [ -z "$CUSTOMIZE_DIR" ]; then
 	usage
 	exit 1
 fi
 
-if true; then #KLDEBUG, KLSKIP 
+if false; then #KLDEBUG, KLSKIP 
 
 echo "Mounting ISO image..."
 mkdir -p "$ISO_MOUNT_DIR" || failure "Cannot create directory $ISO_MOUNT_DIR, error=$?"
@@ -70,7 +84,7 @@ echo "Copying data to remastering root directory..."
 cp -a "$SQUASHFS_MOUNT_DIR" "$REMASTER_DIR" || failure "Cannot copy files from $SQUASHFS_MOUNT_DIR to $REMASTER_DIR, error=$?"
 
 echo "Copying customization files..."
-cp -a "$CUTOMIZE_DIR" "$REMASTER_CUSTOMIZE_DIR" || failure "Cannot copy files from $CUTOMIZE_DIR to $REMASTER_CUSTOMIZE_DIR, error=$?"
+cp -a "$CUSTOMIZE_DIR" "$REMASTER_CUSTOMIZE_DIR" || failure "Cannot copy files from $CUSTOMIZE_DIR to $REMASTER_CUSTOMIZE_DIR, error=$?"
 
 #resolv.conf not present
 #RESOLV_CONF_BACKUP="$REMASTER_DIR/etc/resolv.conf.remaster-original"
@@ -95,6 +109,11 @@ fi
 echo "Running customization script..."
 chroot "$REMASTER_DIR" "/$CUSTOMIZATION_SCRIPT" || failure "Running customization script failed, error=$?"
 echo "Customization script finished"
+
+echo "Pausing for manual customization, press Enter when finished..."
+read DUMMY
+
+fi #KLSKIP
 
 echo "Saving apt cache"
 if [ -e "$APT_CACHE_SAVE_DIR" ]; then
@@ -130,7 +149,6 @@ echo "Updating files lists"
 chroot "$REMASTER_DIR" dpkg-query -W --showformat='${Package} ${Version}\n' > "$NEW_FILES_DIR/filesystem.manifest" || failure "Cannot update filesystem.manifest, error=$?"
 cp "$NEW_FILES_DIR/filesystem.manifest" "$NEW_FILES_DIR/filesystem.manifest-desktop" || failure "Failed to copy $NEW_FILES_DIR/filesystem.manifest to $NEW_FILES_DIR/filesystem.manifest-desktop"
 
-fi #KLSKIP
 
 echo "Preparing SquashFS image"
 mksquashfs "$REMASTER_DIR" "$NEW_FILES_DIR/filesystem.squashfs" || failure "Failed to create squashfs image to NEW_FILES_DIR/filesystem.squashfs, error=$?"
@@ -141,8 +159,8 @@ removeDirectory "$REMASTER_DIR"
 
 echo "Updating locale"
 
-if [ -e "$CUTOMIZE_DIR/livecd_locale" ]; then
-	LIVECD_LOCALE=`cat "$CUTOMIZE_DIR/livecd_locale"`
+if [ -e "$CUSTOMIZE_DIR/livecd_locale" ]; then
+	LIVECD_LOCALE=`cat "$CUSTOMIZE_DIR/livecd_locale"`
 	cat "$ISO_MOUNT_DIR/isolinux/isolinux.cfg" | sed "s#\<append\>#append debian-installer/locale=$LIVECD_LOCALE#g" >"$NEW_FILES_DIR/isolinux.cfg"
 	RESULT=$?
 	if [ $RESULT -ne 0 ]; then
@@ -183,17 +201,18 @@ done
 popd
 
 echo "Creating ISO image"    
-#Creating ISO
-#Overridden files: casper/filesystem.squashfs casper/filesystem.manifest casper/filesystem.manifest-desktop
 
 cp -a "$ISO_MOUNT_DIR/isolinux/isolinux.bin" "$NEW_FILES_DIR/" || failure "Error copying isolinux.bin, error=$?"
+#cp -a "$ISO_MOUNT_DIR/isolinux/pl.tr" "$NEW_FILES_DIR/en.tr" || failure "Error copying pl.tr to en.tr, error=$?"
 
 REPLACED_PATHS="-x $ISO_MOUNT_DIR/casper/filesystem.squashfs -x $ISO_MOUNT_DIR/casper/filesystem.manifest -x $ISO_MOUNT_DIR/casper/filesystem.manifest-desktop"
 CHANGED_PATHS="casper/filesystem.squashfs=$NEW_FILES_DIR/filesystem.squashfs casper/filesystem.manifest=$NEW_FILES_DIR/filesystem.manifest  casper/filesystem.manifest-desktop=$NEW_FILES_DIR/filesystem.manifest-desktop"
 REPLACED_PATHS="$REPLACED_PATHS -x $ISO_MOUNT_DIR/isolinux/isolinux.bin -x $ISO_MOUNT_DIR/isolinux/boot.cat"
 CHANGED_PATHS="$CHANGED_PATHS isolinux/isolinux.bin=$NEW_FILES_DIR/isolinux.bin"
-REPLACED_PATHS="$REPLACED_PATHS -x $ISO_MOUNT_DIR/isolinux/isolinux.cfg "
-CHANGED_PATHS="$CHANGED_PATHS isolinux/isolinux.cfg=$NEW_FILES_DIR/isolinux.cfg"
+#REPLACED_PATHS="$REPLACED_PATHS -x $ISO_MOUNT_DIR/isolinux/en.tr "
+#CHANGED_PATHS="$CHANGED_PATHS isolinux/en.tr=$NEW_FILES_DIR/en.tr"
+REPLACED_PATHS="$REPLACED_PATHS -x $ISO_MOUNT_DIR/casper/initrd.gz "
+CHANGED_PATHS="$CHANGED_PATHS casper/initrd.gz=$NEW_FILES_DIR/initrd.gz"
 
 mkisofs -o "$NEW_FILES_DIR/livecd.iso" \
 	-b "isolinux/isolinux.bin" -c "isolinux/boot.cat" \
