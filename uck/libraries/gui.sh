52c8d6c3c17c304d2e611961dd493eca29a278f9
balliano <balliano@6cad917b-6c01-41db-971f-40384d2b65c3>
2008-12-19 07:40:37 +0000

    some license/copyright issues were fixed (thanks to Alessio Treglia)

--- uck/libraries/gui.sh
+++ uck/libraries/gui.sh
@@ -2,21 +2,20 @@
 
 ###################################################################################
 # UCK - Ubuntu Customization Kit                                                  #
-# Copyright (C) 2006-2007 UCK Team                                                #
+# Copyright (C) 2006-2008 UCK Team                                                #
 #                                                                                 #
-# This program is free software; you can redistribute it and/or                   #
-# modify it under the terms of the GNU General Public License                     #
-# as published by the Free Software Foundation; version 2                         #
-# of the License.                                                                 #
+# UCK is free software: you can redistribute it and/or modify                     #
+# it under the terms of the GNU General Public License as published by            #
+# the Free Software Foundation, either version 3 of the License, or               #
+# (at your option) any later version.                                             #
 #                                                                                 #
-# This program is distributed in the hope that it will be useful,                 #
+# UCK is distributed in the hope that it will be useful,                          #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   #
 # GNU General Public License for more details.                                    #
 #                                                                                 #
 # You should have received a copy of the GNU General Public License               #
-# along with this program; if not, write to the Free Software                     #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. #
+# along with UCK.  If not, see <http://www.gnu.org/licenses/>.                    #
 ###################################################################################
 
 function find_dialog()
